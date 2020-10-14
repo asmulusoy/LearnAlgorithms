e1e392030cb8317828cbf0a742a3781cc5d4ff8e
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-10-14 22:39:59 +0300

    Add project files.

---
+++ LearnAlgorithms/PalindromicWords.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnAlgorithms
+{
+    public static class PalindromicWords
+    {
+        public static List<string> GetPalindromicWords(string input)
+        {
+            List<string> words = new List<string>();
+            for (int i = 2; i < input.Length; i++)
+            {
+                string palindromicWord = string.Empty;
+                for (int j = 1; 0 < 1; j++)
+                {
+                    if (i - j > 0 && i + j < input.Length)
+                    {
+                        if (input[i - j] != input[i + j])
+                        {
+                            if (!string.IsNullOrEmpty(palindromicWord))
+                                words.Add(palindromicWord);
+                            break;
+                        }
+                        else
+                        {
+                            palindromicWord = input.Substring(i - j, (j * 2) + 1);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+            return words.OrderByDescending(x => x.Length).ToList();
+        }
+
+    }
+}
---
+++ LearnAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "itisalongestablishedfactthatareadaaderwillbedistractedbythereadablekabakcontenkavaktofaeyedipadanadapideyepagewhenlookingatitslayout.thepointofusingloremipsumisthatithasamore-or-lessnormaldistributionofletters,asopposedtousing'contenthere,contenthere',makingitlooklikereadableenglish.manydesktoppublishingpackagesandwebpageeditorsnowuseloremipsumastheirdefaultmodeltext,andasearchfor'loremipsum'willuncovermanywebsitesstillintheirinfancy.variousversionshaveevolvedovertheyearssometimesbyaccidentsometimesonpurpose";
+            List<string> palindromicWords = PalindromicWords.GetPalindromicWords(input);
+            palindromicWords.ForEach(x => Console.WriteLine(x));
+            Console.ReadLine();
+        }
+
+    }
+}
