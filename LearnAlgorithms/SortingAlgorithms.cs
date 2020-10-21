522dc1e93b883de5552584954ff30eb23a921ba0
kubrasonmezz <sonmezkbra@gmail.com>
2020-10-21 16:07:32 +0300

    Bubble Sort eklendi.

--- LearnAlgorithms/SortingAlgorithms.cs
+++ LearnAlgorithms/SortingAlgorithms.cs
@@ -23,6 +23,23 @@ namespace LearnAlgorithms
             }
             return array.ToList();
         }
+        public static List<int> BubbleSort(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j + 1] < array[j])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array.ToList();
+        }
+
 
     }
 }
